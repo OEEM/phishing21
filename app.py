e2c1336671759d706a959d02949132ecc8174af2
ADITYA SINGH <120800935+OEEM@users.noreply.github.com>
2023-05-17 09:31:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 # col1, col2 = st.columns([1, 3])
 
 st.title('Phishing Website Detection using Machine Learning')
-st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
-         ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
+#st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
+        # ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
 
 
 
